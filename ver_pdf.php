9572928288d37cb5c38812860573da193c370deb
marcelo <marcelo@marce-pc.(none)>
2015-05-09 11:30:17 -0400

    indice dela ayuda y el visor de pdf. falta mejorar el indice y capturar el error a la primera cargada de la ayuda

--- ver_pdf.php
+++ ver_pdf.php
@@ -1,34 +1,25 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Ver Libro</title>
-<style type="text/css">
-body {
-	font: 100%/1.4 Verdana, Arial, Helvetica, sans-serif;
-	background-color: #42413C;
-	margin: 0;
-	padding: 0;
-}
-.visor {
-	float: left;
-	width: 80%;
-	height:700px;
-	background-color: #FFF;
-	padding-bottom: 10px;
-}
-</style>
-</head>
-
-<body>
-
-<div class="container">
-	
-   <div class="visor" >
-   	
-   	<embed src='archivos/manual.pdf' width=150% height=70% aligen align="left"/>
-	
+<?php
+$titulo="Ayuda del Sistema de Apoyo a la Empresa TIS";
+include("conexion/verificar_gestion.php");
+session_start();
+include('header.php');
+?>
+<!--
+<div class="container-fluid">
+	<div class="row-fluid">
+       <div class="span12">
+       	<div class="visor">
+    	<?php
+			$id=$_GET['nom_arch'];
+			//buscar por id en la tabla
+			 echo "<embed src='archivos/".$id.".pdf' width=800 height=500 />"//no id si no nombre del archivo
+	  	?>
 	</div>
+	</div>
+	</div>	
+
 </div>
-</body>
-</html>
+-->
+<?php
+ // include('footer.php');
+ ?>
